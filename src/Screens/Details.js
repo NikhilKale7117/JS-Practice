ad9f03098137a39b5fbd37d0913f18d8044cf338
NikhiKale7117 <nikhilkale7117@gmail.com>
2021-08-26 17:54:27 +0530

    first commit

--- src/Screens/Details.js
+++ src/Screens/Details.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React, {useState} from 'react';
 import { Button, ScrollView, StyleSheet, Text, TouchableOpacity, View , Dimensions} from 'react-native'
 import ImageDeatails from '../../components/ImageDeatails'
 
